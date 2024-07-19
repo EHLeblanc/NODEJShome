856990e6abc79672e19f1343ff5d035258daf10b
Eli LeBlanc <E1leblanc@outlook.com>
2024-07-19 16:08:46 -0500

    foreach push

---
+++ src/forEach.js
@@ -0,0 +1,8 @@
+// src/forEach.js
+function forEach(items, callback) {
+    for (let index = 0; index < items.length; index++) {
+        callback(items[index]);
+    }
+}
+
+module.exports = forEach;
